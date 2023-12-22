497e7ea50c543a3e673d1f79771d7acd9786a117
leonardoolia <leonardooliaa@gmail.com>
2023-12-22 17:03:02 +0100

    created random numbers to put in the array

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -12,6 +12,13 @@ console.log(userNumbers);
 const arrayElements = [];
 console.log(arrayElements);
 
+for (let i = 1; i <= userNumbers; i++) {
+    //? 3. Generare numeri casuali da 1 a 100
+    const randomNumbers = Math.floor(Math.random() * 100) + 1;
+    //? 4. Inserire i numeri casuali all'interno dell'array
+    arrayElements.push(randomNumbers);
+};
+
 
 
 
