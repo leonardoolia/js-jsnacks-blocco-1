8d51eaa353f9bbb57baec90941ddb08bc44cb624
leonardoolia <leonardooliaa@gmail.com>
2024-01-08 12:09:20 +0100

    fixed bonus

--- array_bonus/script.js
+++ array_bonus/script.js
@@ -1,29 +1,11 @@
 /* Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro. */
 
-/*
-
-STEP
-
-1. Individuare quale array è più lungo e quale è più corto
-
-2. Generare numeri casuali tanti quanti ne mancano al più corto
-
-
-*/
 
 const array1 = [5, 7];
 const array2 = [3, 54, 6, 3, 23];
 
-
-// STEP 1
-
-const shorterArray = array1.length > array2.length ? array2 : array1;
-const longerArray = array1.length > array2.length ? array1 : array2;
-
-// STEP 2
-
-while (shorterArray.length !== longerArray.length) {
+while (array1.length < array2.length) {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     shorterArray.push(randomNumber);
 };
